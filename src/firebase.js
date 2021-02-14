b797f7caf62fee87ddad31f8a080c55670cb55fa
J o s e p h  S a m i r <68662601+josephcmd@users.noreply.github.com>
2021-02-14 17:05:49 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import "firebase/firestore";
 import 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDTQQOt4FXxChzZBB7uaK02EDIl2M5bgzI",
-    authDomain: "fancy-todo-38543.firebaseapp.com",
-    databaseURL: "https://fancy-todo-38543.firebaseio.com",
-    projectId: "fancy-todo-38543",
-    storageBucket: "fancy-todo-38543.appspot.com",
-    messagingSenderId: "933278068562",
-    appId: "1:933278068562:web:eb315f427fc78e1e95d48b"
+    apiKey: "key",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 firebase.initializeApp(firebaseConfig)
